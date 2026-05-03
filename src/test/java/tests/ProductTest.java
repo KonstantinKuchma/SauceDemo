164adb4e779da50f98b21c9c25668668259f2a91
KonstantinKuchma <knstntnkuchma@gmail.com>
2026-05-03 11:06:00 +0300

    add new homework for SauseDemo

--- src/test/java/tests/ProductTest.java
+++ src/test/java/tests/ProductTest.java
@@ -2,10 +2,12 @@ package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import tests.base.BaseTest;
 
 public class ProductTest extends BaseTest {
 
-    @Test
+    @Test(description = "Проверка возможности просмотра карточки товара",
+            testName = "Проверка возможности просмотра карточки товара")
     public void checkProductCart() {
         loginPage.open();//вызываем метод, который открывает страницу
         loginPage.login("standard_user", "secret_sauce");//авт c лог "standard_user" и пар "secret_sauce"
